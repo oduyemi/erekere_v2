fd88074f4dc6deb2fa3c2dfc4d239e97e63f44b0
Yemi Oduyemi <yhermii@gmail.com>
2026-04-16 13:14:03 +0100

    instance config setup changed

--- ereapp/__init__.py
+++ ereapp/__init__.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask
 from flask_pymongo import PyMongo
 from markupsafe import Markup
 
 starter = Flask(__name__, instance_relative_config=True)
-starter.config.from_pyfile('config.py', silent=False)
+starter.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+starter.config['MONGO_URI'] = os.getenv('MONGO_URI')
+starter.config['CURRENCY'] = os.getenv('CURRENCY')
+# starter.config.from_pyfile('config.py', silent=False)
 
 mongo = PyMongo(starter)
+
+if not starter.config['MONGO_URI']:
+    raise ValueError("MONGO_URI is not set")
 
 from ereapp import adminroutes, userroutes
 
